9d16b9443eee795882a5df1f4e5dffed18c0bfbb
shreya-celestial <toddlercoding@gmail.com>
2023-11-23 17:31:35 +0530

    adding events list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import { allEvents } from '../apis.jsx';
+import styles from '../styleModules/Home.module.css'
+import EventsList from "./EventsList.jsx";
+
 const Home = ({ user }) => {
+    const [isPending, setIsPending] = useState(true);
+    const [events, setEvents] = useState(null);
+
+    useEffect(() => {
+        const data = async () => {
+            const events = await allEvents(user);
+            setIsPending(false);
+            setEvents(events);
+        }
+        data();
+    }, []);
+
     return (
-        <>hello</>
+        <>
+            {isPending && <h1 className={styles.loading}>Loading...</h1>}
+            {events && <EventsList events={events} />}
+        </>
     );
 }
 
