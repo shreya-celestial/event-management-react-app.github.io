4e1e5fb69eb130cb5cd0502a855a9ead7b2bba2f
shreya-celestial <toddlercoding@gmail.com>
2023-11-24 12:19:26 +0530

    adding new event page

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -4,6 +4,7 @@ import NavBar from './NavBar';
 import Home from './Home';
 import Event from './Event';
 import EditEvent from './EditEvent';
+import CreateEvent from './CreateEvent';
 
 const UserPage = ({ user }) => {
     return (
@@ -12,7 +13,7 @@ const UserPage = ({ user }) => {
             <div className={styles.container}>
                 <Routes>
                     <Route exact path='/' element={<Home user={user} />} />
-                    <Route exact path='/create' element={<>create</>} />
+                    <Route exact path='/create' element={<CreateEvent />} />
                     <Route exact path='/:id' element={<Event />} />
                     <Route exact path='/:id/edit' element={<EditEvent />} />
                 </Routes>
